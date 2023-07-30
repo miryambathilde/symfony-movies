a083e00f21eba3b17ed0f3cf3e4b120969d22389
Miryam Bathilde <mbathilde@crescenta.com>
2023-07-30 13:58:46 +0200

    twig template views installed & added to project

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,17 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+# use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'app_movies', methods: ['GET', 'HEAD'])]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+      return $this->render('movies/index.html.twig');
     }
 }
