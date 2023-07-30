b23f15be4bb6625122e3ecdb3f5d31fc109c03d3
Miryam Bathilde <mbathilde@crescenta.com>
2023-07-30 14:45:28 +0200

    list movies get view

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-# use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+    }
+
     #[Route('/movies', name: 'app_movies', methods: ['GET', 'HEAD'])]
     public function index(): Response
     {
-      return $this->render('movies/index.html.twig');
+        $movies = $this->entityManager->getRepository(Movie::class)->findAll();
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies,
+        ]);
     }
 }
