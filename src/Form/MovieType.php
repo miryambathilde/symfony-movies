de4c41eedd0798c88ea922ac9f5c52ca5a0aba1d
Miryam Bathilde <mbathilde@crescenta.com>
2023-07-30 15:41:58 +0200

    symfony form installed and created the form for Movie entity

---
+++ src/Form/MovieType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('runtime')
+            ->add('budget')
+            ->add('poster')
+            ->add('release_date')
+            ->add('genre')
+            ->add('country')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
